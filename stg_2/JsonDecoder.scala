5502fe46d25ad8b46b56aabc801e1d6f351740a3
vladimir <vi.vova2000@gmail.com>
2020-08-21 17:24:11 +0300

    stg_2_2

--- stg_2/JsonDecoder.scala
+++ stg_2/JsonDecoder.scala
@@ -1,36 +1,45 @@
-import org.apache.spark.sql.SparkSession
+import java.io.{BufferedWriter, FileWriter}
+
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
-import org.json4s.jackson.Serialization
 
 import scala.io.Source
-import scala.util.Try
 
-object JsonDecoder extends App{
+object JsonDecoder extends App {
+  val source: String =
+    Source.fromFile("./datasource/milano-grid.geojson")
+      .getLines
+      .mkString
 
-    val source: String =
-        Source.fromFile("./datasource/milano-grid.geojson")
-          .getLines
-          .mkString
+  val file = "./datasource/new_file_grid.csv"
+  val writer = new BufferedWriter(new FileWriter(file))
 
-    val json = parse(source) \ "features"
+  val json = parse(source) \ "features"
 
-   val cellId = for {
-        JObject(properties) <- json
-        JField("cellId", JInt(cellId)) <- properties
-    } yield cellId
+  val cellId = for {
+    JObject(properties) <- json
+    JField("cellId", JInt(cellId)) <- properties
+  } yield cellId
 
-   val coor = for {
-       JObject(geometry) <- json
-       JField("coordinates", JArray(coordinates)) <- geometry
-   } yield coordinates
+  val coor = for {
+    JObject(geometry) <- json
+    JField("coordinates", JArray(coordinates)) <- geometry
+  } yield coordinates
 
-    implicit val formats = DefaultFormats
+  val miGrid = cellId.zip(
+    coor.map {
+      case List(JArray(list)) =>
+        list.map {
+          case JArray(list1) => list1
+        }
+      case _ => None
+    }).map {
+    case (square_id, coordinates) => (square_id, "[0-9]*\\.[0-9]*".r.findAllIn(coordinates.toString).mkString(","))
+  }
 
-    println(cellId.zip(coor))
-
-
-
-
-
+  Range(1, 10000).zip(miGrid.map {
+    case (square_id, str) => writer.write(s"$square_id,$str\n")
+  })
+  writer.close()
 }
+
